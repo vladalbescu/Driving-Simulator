61e21d62ed3eda223517bbfd1f5a38ff36ba69fc
Vlad Albescu <vlad@agrocity.eu>
2020-12-23 14:22:10 +0200

    Added vehicle + obstacles to the scene & created scripts for player control and camera movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 20f;
+    private float turnSpeed = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // This is where we get player input
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float forwardInput = Input.GetAxis("Vertical");
+
+        // Moves the card forward based on vertical input
+        transform.Translate(Vector3.forward * speed * forwardInput * Time.deltaTime);
+
+        // Rotates the car based on horizontal input
+        transform.Rotate(Vector3.up * turnSpeed * horizontalInput * Time.deltaTime);
+    }
+}
